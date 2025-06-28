31efba3316fb4251890870234fdcc4b78e499b18
khanhsaan <109020045+khanhsaan@users.noreply.github.com>
2025-06-29 01:17:09 +1000

    Create header and header style

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
     setTimeout(() => setIsLoading(false), 1500);
   }, []);
 
-  // Set loading screen and loading wheel
+  // if the loading state is true, render loading screen and loading wheel
   if(isLoading) {
     return (
       <div className='loading-screen'>
@@ -71,19 +71,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Header */}
+      <header className="app-header">
+        <div className='header-content'></div>
       </header>
     </div>
   );
